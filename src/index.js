32ac5e56c936aaaa25ce333ae5c8cefab2f5cdad
alikonuk1 <alikonuk1@gmail.com>
2024-10-23 22:25:17 +0300

    Add analytics

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
           <App />
+          <Analytics/>
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root")
